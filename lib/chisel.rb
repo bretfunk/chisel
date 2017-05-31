6a523dbfed94a2fb12e49412b9c9cdaa84fb3aad
BFunk <bret.e.funk@gmail.com>
2017-05-31 13:36:19 -0600

    testing basic ideas

---
+++ lib/chisel.rb
@@ -0,0 +1,32 @@
+#$ ruby ./lib/chisel.rb my_input.markdown my_output.html
+
+#read in my_input markdown
+for arg in ARGV
+   puts arg
+end
+
+#covert file
+
+
+#create my_output
+new_file = File.open(<filename>, "w")
+new_file.write("text")
+
+
+class MarkdownCoverter
+
+File.open(ARGV[0], "w"){ |somefile| somefile.puts "Hello file!"}
+
+
+contents = File.open("sample.txt", "r"){ |file| file.read }
+puts contents
+
+
+
+
+
+
+end
+
+#output
+puts "Converted my_input.markdown (6 lines) to my_output.html (8 lines)"
