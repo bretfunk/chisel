bae759d24574cc198c895fafc2d813633ce08f32
BFunk <bret.e.funk@gmail.com>
2017-05-31 14:56:46 -0600

    added methods to chisel.rb to break up logic

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -7,33 +7,40 @@ convert_file = my_input.read
 my_input.close
 
 #convert file
+def convert_files(text)
 filtered_text = ""
-convert_file.split("\n").each do |sentence|
-  if sentence[0..1] == "##"
-    filtered_text << "<h1>"
-    filtered_text << sentence
-    filtered_text << "</h1>"
-  elsif sentence[0] == "#"
-    filtered_text << "<h2>"
-    filtered_text << sentence
-    filtered_text << "</h2>"
-  else
-    filtered_text << "<p>"
-    filtered_text << sentence
-    filtered_text << "</p>"
+text.split("\n").each do |sentence|
+    if sentence[0..1] == "##"
+      filtered_text << "<h1>"
+      filtered_text << sentence
+      filtered_text << "</h1>"
+    elsif sentence[0] == "#"
+      filtered_text << "<h2>"
+      filtered_text << sentence
+      filtered_text << "</h2>"
+    else
+      filtered_text << "<p>"
+      filtered_text << sentence
+      filtered_text << "</p>"
+    end
   end
+  filtered_text
+end
+
+def filter_heading_one(sentence)
+end
+def filter_heading_two(sentence)
+end
+def filter_bold(sentence)
+end
+def filter_italics(sentence)
 end
 
 #output file
 my_output_count = 0
 my_output = File.open(ARGV[1], 'w')
-my_output.write(filtered_text)
+my_output.write(convert_files(convert_file))
 my_output.close
 
 #output info
 puts "Converted my_input.markdown (#{my_input_count} lines) to my_output.html (#{my_output_count} lines)"
-
-
-#class MarkdownCoverter
-
-# end
